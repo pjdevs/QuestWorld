a104e382e9bf0cc2222951ba971fb6ede56e964e
pjdevs <pj33720@gmail.com>
2025-10-24 09:23:08 +0200

    fix(ability): real jump ability task in cpp

---
+++ Plugins/GameplayAbilitySystemPlugin/Source/GameplayAbilitySystemPlugin/Private/Tasks/AbilityTask_JumpAndWaitForLanding.cpp
@@ -0,0 +1,51 @@
+﻿// Copyright pjdevs. All Rights Reserved.
+
+
+#include "Tasks/AbilityTask_JumpAndWaitForLanding.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+
+UAbilityTask_JumpAndWaitForLanding* UAbilityTask_JumpAndWaitForLanding::JumpAndWaitForLanding(
+	UGameplayAbility* OwningAbility
+)
+{
+	return NewAbilityTask<UAbilityTask_JumpAndWaitForLanding>(OwningAbility);
+}
+
+void UAbilityTask_JumpAndWaitForLanding::Activate()
+{
+	ACharacter* Character = Cast<ACharacter>(GetAvatarActor());
+
+	if (!Character)
+	{
+		EndTask();
+		return;
+	}
+
+	Character->Jump();
+	Character->MovementModeChangedDelegate.AddDynamic(this, &UAbilityTask_JumpAndWaitForLanding::OnMovementModeChanged);
+}
+
+void UAbilityTask_JumpAndWaitForLanding::OnMovementModeChanged(
+	ACharacter* Character,
+	EMovementMode PrevMovementMode,
+	uint8 PreviousCustomMode
+)
+{
+	if (Character->GetCharacterMovement()->MovementMode != MOVE_Falling)
+	{
+		OnLanded.Broadcast();
+		CleanAndEndTask();
+	}
+}
+
+void UAbilityTask_JumpAndWaitForLanding::CleanAndEndTask()
+{
+	if (ACharacter* Character = Cast<ACharacter>(GetAvatarActor()))
+	{
+		Character->MovementModeChangedDelegate.RemoveDynamic(this, &UAbilityTask_JumpAndWaitForLanding::OnMovementModeChanged);
+	}
+
+	EndTask();
+}
---
+++ Plugins/GameplayAbilitySystemPlugin/Source/GameplayAbilitySystemPlugin/Public/Tasks/AbilityTask_JumpAndWaitForLanding.h
@@ -0,0 +1,44 @@
+﻿// Copyright pjdevs. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/Tasks/AbilityTask.h"
+#include "AbilityTask_JumpAndWaitForLanding.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnLandedDelegate);
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEPLAYABILITYSYSTEMPLUGIN_API UAbilityTask_JumpAndWaitForLanding : public UAbilityTask
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(BlueprintAssignable)
+	FOnLandedDelegate OnLanded;
+
+	/** Start jump and wait for landing */
+	UFUNCTION(
+		BlueprintCallable,
+		Category = "Ability|Tasks",
+		meta = (
+			DisplayName = "Jump And Wait For Landing",
+			HidePin = "OwningAbility",
+			DefaultToSelf = "OwningAbility",
+			BlueprintInternalUseOnly = "TRUE"
+		)
+	)
+	static UAbilityTask_JumpAndWaitForLanding* JumpAndWaitForLanding(UGameplayAbility* OwningAbility);
+
+protected:
+	virtual void Activate() override;
+
+private:
+	UFUNCTION()
+	void OnMovementModeChanged(ACharacter* Character, EMovementMode PrevMovementMode, uint8 PreviousCustomMode);
+
+	void CleanAndEndTask();
+};
